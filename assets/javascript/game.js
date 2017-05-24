8792543d6183ade94cec60bed3ed2b52c819f78a
Michael Piatnichko <mpiatnichko@gmail.com>
2017-05-24 11:01:36 -0600

    HTML setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+//the player is shown a random number between 19-120 at the start of the game
+//when crystal is clicked an amount of points will be added to total score between 1-12.
+//update the score when crystal is clicked
+//player wins if thier total score matches the random number
+//player loses if thier score is above random number
+//game restarts when the player wins or loses
