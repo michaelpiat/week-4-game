d7dfba1e9073c9336e4adf278eb7552b6d5f4f1a
Michael Piatnichko <mpiatnichko@gmail.com>
2017-05-24 17:12:18 -0600

    changes to HTML and update functions on javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,27 +15,71 @@ var crystalNumbers = [];
 
 
 //creates 4 number between 1-12 and puts them in an array called crystal numbers
-function crystalNumberGenerator() {
+	function crystalNumberGenerator() {
 
-	for (var i = 0; i < 4; i++) {
-		var numbers = Math.floor(Math.random()*12 + 1);
-		crystalNumbers.push(numbers);
+		for (var i = 0; i < 4; i++) {
+			var numbers = Math.floor(Math.random()*12 + 1);
+			crystalNumbers.push(numbers);
+		}
+
+		console.log(crystalNumbers);
 	}
 
-console.log(crystalNumbers);
-
-}
-
-crystalNumberGenerator();
+	crystalNumberGenerator();
 
 //function that creates a random number between 19 and 120.
-function randomNumber(min, max) {
-	 var gameNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-	 return gameNumber;
-}
+	function randomNumber(min, max) {
+	 	
+	 	return Math.floor(Math.random() * (max - min + 1)) + min; 
+	}
 
 //places the random number in the HTML
 $("#randomNumber").text(randomNumber(19, 120));
+
+//this function takes a number from the crystal numbers array and assigns it to a crystal.
+function crystallMaker(crystalPosition) {
+	playerPoints = playerPoints + crystalNumbers[crystalPosition];
+		console.log(playerPoints);
+//targets the playerScore id and changes it with the players score		
+		$("#playerScore").text(playerPoints);	
+
+}
+//crystal 1 gets the number at index 0 of crystal numbers array
+	$(".crystal1").on("click", function() {
+	crystallMaker(0);
+})
+
+
+//crystal 1 gets the number at index 1 of crystal numbers array
+	$(".crystal2").on("click", function() {	
+	crystallMaker(1);	
+})
+
+
+
+//crystal 1 gets the number at index 2 of crystal numbers array
+	$(".crystal3").on("click", function() {
+	crystallMaker(2);
+})
+
+
+//crystal 1 gets the number at index 3 of crystal numbers array
+	$(".crystal4").on("click", function() {
+	crystallMaker(3);
+})
+
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
 
